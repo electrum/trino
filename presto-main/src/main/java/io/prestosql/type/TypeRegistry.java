prestodb/presto

Add explicit support for type constructors
This adds an optional type constructor operator which can be used by
types that have a literal form that is different than casting from
varchar. Currently, the JSON type is the only such instance and
support for it was previously hard-coded in several places.

presto-main/src/main/java/com/facebook/presto/type/TypeRegistry.java
@@ -665,6 +665,13 @@ public final class TypeRegistry
         return functionRegistry.getScalarFunctionImplementation(functionRegistry.resolveOperator(operatorType, argumentTypes)).getMethodHandle();
     }
 
+    @Override
+    public MethodHandle resolveConstructor(Type type)
+    {
+        checkState(functionRegistry != null, "functionRegistry was not set");
+        return functionRegistry.getScalarFunctionImplementation(functionRegistry.resolveConstructor(type)).getMethodHandle();
+    }
+
     public static class TypeCompatibility
     {
         private final Optional<Type> commonSuperType;

electrum/trino

presto-main/src/main/java/io/prestosql/type/TypeRegistry.java
@@ -666,6 +666,13 @@ public final class TypeRegistry
         return functionManager.getScalarFunctionImplementation(functionManager.resolveOperator(operatorType, fromTypes(argumentTypes))).getMethodHandle();
     }
 
+    @Override
+    public MethodHandle resolveConstructor(Type type)
+    {
+        checkState(functionManager != null, "functionManager was not set");
+        return functionManager.getScalarFunctionImplementation(functionManager.lookupConstructor(type.getTypeSignature())).getMethodHandle();
+    }
+
     public static class TypeCompatibility
     {
         private final Optional<Type> commonSuperType;